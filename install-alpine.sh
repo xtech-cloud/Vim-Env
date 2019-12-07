158f765b9d693049b5f362e1d1990455ee079add
root <root@EL-XPS.localdomain>
2019-12-07 17:20:08 +0000

    fixed

--- install-alpine.sh
+++ install-alpine.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.vim
 rm -rf ~/.vim/bundle
 cp -rf bundle ~/.vim/
-cp -rf dep/alpine/gotools/* /usr/local/bin/
+cp -rf dep/alpine/gotools/* /usr/bin/
 cp -rf dep/alpine/clang/* /usr/bin/
 
 cd dep/alpine/ctags
